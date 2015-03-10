4948c4a5459eecdcfdbaa8a616a8c8013420ea00
Pablo Soldi <soldi.pablo@gmail.com>
2015-03-09 21:36:24 -0300

    Add Gemfile

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'hobbit'
+gem 'hobbit-contrib', require: 'hobbit/contrib'
+gem 'rack-protection'
+gem 'nokogiri'
+gem 'tilt'
+
+require 'open-uri'
--- posiciones.rb
+++ posiciones.rb
@@ -1,13 +1,14 @@
-require 'hobbit'
-require 'hobbit/contrib'
-require 'nokogiri'
-require 'open-uri'
+require 'bundler'
+require 'securerandom'
+
+Bundler.require :default, ENV['RACK_ENV'].to_sym
+
 
 Equipo = Struct.new(:nombre, :puntos, :partidos, :ganados, :empatados, :perdidos, :goles_favor, :goles_contra, :diferencia) do
   def puntos_sobre_partidos
     puntos.fdiv(partidos)
   end
-  
+
   def ganados_sobre_partidos
     ganados.fdiv(partidos)
   end
@@ -24,22 +25,23 @@ end
 
 class App < Hobbit::Base
   include Hobbit::Render
-  
+  use Rack::Session::Cookie, secret: SecureRandom.hex(64)
+  use Rack::Protection
   use Rack::Static, root: 'public', urls: ['/css']
 
   get '/' do
     equipos=[]
     punteros=[]
     urls = [ "http://www.ahba.com.ar/clubes/index.php?accion=mostrar_tabla_posiciones&sector=damas&categoria=Campeonato&id_torneo=61",      "http://www.ahba.com.ar/clubes/index.php?accion=mostrar_tabla_posiciones&sector=damas&categoria=Campeonato&id_torneo=68","http://www.ahba.com.ar/clubes/index.php?accion=mostrar_tabla_posiciones&sector=damas&categoria=Campeonato&id_torneo=143","http://www.ahba.com.ar/clubes/index.php?accion=mostrar_tabla_posiciones&sector=damas&categoria=Campeonato&id_torneo=150","http://www.ahba.com.ar/clubes/index.php?accion=mostrar_tabla_posiciones&sector=damas&categoria=Campeonato&id_torneo=157"]
-    
+
     urls.each do |url|
       @doc = Nokogiri::HTML(open(url))
       cells = @doc.css('html body table tr td div.aplicacion table.showpanel tbody tr').search('tr')[0..-1]
-      
+
       first = true
       cells.each do |cell|
         equipo = Equipo.new(cell.search('td')[0].text, cell.search('td')[1].text.to_i, cell.search('td')[2].text.to_i, cell.search('td')[3].text.to_i, cell.search('td')[4].text.to_i, cell.search('td')[5].text.to_i, cell.search('td')[7].text.to_i, cell.search('td')[8].text.to_i, cell.search('td')[9].text.to_i)
-        
+
         if first
           punteros << equipo
           first = false
